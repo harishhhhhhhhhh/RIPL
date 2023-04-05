ebbb9a19de2695ee09d8b442470689781d794535
harishhhhhhhhhh <hari66927@gmail.com>
2023-04-05 13:55:46 +0530

    player controller modified

--- backend/app/Http/Controllers/playerController.php
+++ backend/app/Http/Controllers/playerController.php
@@ -16,62 +16,21 @@ class playerController extends Controller
             ->select('id','name', 'skill')
             ->whereNotIn('id', $ids)
             ->get();
-        $ids = DB::table('playerteam')
-            ->pluck('playerid')
-            ->toArray();
-        $players = DB::table('playerdetails')
-            ->select('id','name', 'skill')
-            ->whereNotIn('id', $ids)
-            ->get();
         return response()->json($players);
 
 
     }
 
     public function getDataBasedOnTeam(Request $request){
-        //$team = $request->input('teamName');
-        // $teamid = DB::table('teamdetails')
-        //     ->where('teamName', $team)
-        //     ->pluck('id')
-        //     ->first();
-        // $playerids = DB::table('playerteam')
-        //      ->where('teamid', $teamid)
-        //      ->pluck('playerid')
-        //      ->toArray();
-        // $players = DB::table('playerdetails')
-        //    ->whereIn('id', $playerids)
-        //    ->select('name', 'skill')
-        //    ->get();
-        // return response()->json($players);
-
-        //$team = $request->input('teamName');
-        // $teamid = DB::table('teamdetails')
-        //     ->where('teamName', $team)
-        //     ->pluck('id')
-        //     ->first();
-        // $playerids = DB::table('playerteam')
-        //      ->where('teamid', $teamid)
-        //      ->pluck('playerid')
-        //      ->toArray();
-        // $players = DB::table('playerdetails')
-        //    ->whereIn('id', $playerids)
-        //    ->select('name', 'skill')
-        //    ->get();
-        // return response()->json($players);
-
         $team=$request->input('teamName');
+
         $players =DB::table('playerdetails as pd')
         ->select('pd.name','pd.skill','td.teamOwners','td.teamCaptain')
         ->join('playerteam as pt', 'pd.id', '=', 'pt.playerid')
         ->join('teamdetails as td', 'pt.teamid', '=', 'td.id')
         ->where('td.teamName', '=', $team)
         ->get();
-        $players =DB::table('playerdetails as pd')
-        ->select('pd.name','pd.skill','td.teamOwners','td.teamCaptain')
-        ->join('playerteam as pt', 'pd.id', '=', 'pt.playerid')
-        ->join('teamdetails as td', 'pt.teamid', '=', 'td.id')
-        ->where('td.teamName', '=', $team)
-        ->get();
+      
         return response()->json($players);
     }
 
